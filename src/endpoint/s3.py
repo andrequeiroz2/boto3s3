bac6933ea8ebfbebcd57df0aebd8de812fe0626f
andrequeiroz2 <andrequeiroz.com@gmail.com>
2022-09-25 11:36:44 -0300

    init

---
+++ src/endpoint/s3.py
@@ -0,0 +1,24 @@
+from fastapi_utils.inferring_router import InferringRouter
+from fastapi_utils.cbv import cbv
+from business.s3 import S3
+from schema.s3 import BucketLocationPostSchema, BucketListSchema, BucketExistSchema
+from fastapi import UploadFile, Depends
+
+s3_router = InferringRouter()
+
+
+@cbv(s3_router)
+class S3Router:
+
+    @s3_router.get("/all/")
+    async def get_all(self) -> BucketListSchema:
+        return await S3().get_all()
+
+    @s3_router.get("/exist/{user_guid}/")
+    async def get_exist(self, user_guid: str) -> BucketExistSchema:
+        return await S3().is_exist_path(user_guid)
+
+    @s3_router.post("/{user_guid}/")
+    async def post_bucket(self, image: UploadFile, path_name: str, user_guid: str):
+        await S3().uploadfile(image, user_guid, path_name)
+        return ""
